60f10402f759c86a0d0f286f1e28c87b5680ce4c
Jose Manuel Alvarez Guzman <128666485+tepachepegajoso@users.noreply.github.com>
2025-04-08 14:01:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,22 +4,22 @@ from firebase_admin import credentials, firestore
 from PIL import Image
 import time
 
-# Inicializar Firebase desde st.secrets
+
 if not firebase_admin._apps:
-    firebase_info = dict(st.secrets["firebase"])  # 👈 convierte los secrets a dict
+    firebase_info = dict(st.secrets["firebase"])  
     cred = credentials.Certificate(firebase_info)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-# Parámetros
+
 collection_name = "KISS"
 document_name = "CLc5jpvARY1LOvqjZWpx"
 campo = "Cantidad"
 
 st.title("¿Quieres un besito? 👀💘")
 
-# Leer el valor actual desde Firestore
+
 doc_ref = db.collection(collection_name).document(document_name)
 doc = doc_ref.get()
 
@@ -31,17 +31,17 @@ else:
 
 st.write(f"Besitos que te debo: **{cantidad_actual}**")
 
-# Botón para incrementar
 if st.button("¡Quiero besito!"):
     nueva_cantidad = cantidad_actual + 1
     doc_ref.update({campo: nueva_cantidad})
     
     st.success(f"Cantidad de besitos actualizada a {nueva_cantidad}")
-    
-    st.image("Aq.gif", caption="¡Listo!", use_column_width=True)
+
+    st.image("Aq.gif", caption="¡Listo!", use_container_width=True)
     
     time.sleep(2.9)
     st.rerun()
+
 
     
     st.image("Aq.gif", caption="¡Listo!", use_column_width=True)
